da167a71648107f13469f92be7aee1ea7132dba2
Mychnik <mmanowiecki01@gmail.com>
2021-02-26 13:18:27 +0100

    added repository interfaces

---
+++ back-end/ForumSolution/Data.Forum/Interfaces/ICategory/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Data.Forum.Entities;
+using Data.Forum.Interfaces.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Forum.Interfaces.ICategory
+{
+    public interface ICategoryRepository : IAsyncRepository<Category>
+    {
+    }
+
+    
+}
