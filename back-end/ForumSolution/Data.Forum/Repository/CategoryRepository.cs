c6cb228676edfa48cf3f453f77f5b50551002240
Mychnik <mmanowiecki01@gmail.com>
2021-02-27 01:09:03 +0100

    base repository implemented

---
+++ back-end/ForumSolution/Data.Forum/Repository/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using Data.Forum.Entities;
+using Data.Forum.Interfaces.ICategory;
+using Data.Forum.Interfaces.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Forum.Repository
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ForumDbContext dbContext) :base(dbContext)
+        {
+
+        }
+        public Task<Category> AddAsync(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Category> IAsyncRepository<Category>.GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IReadOnlyList<Category>> IAsyncRepository<Category>.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ back-end/ForumSolution/Data.Forum/Repository/PostRepository.cs
@@ -0,0 +1,42 @@
+﻿using Data.Forum.Entities;
+using Data.Forum.Interfaces.IPost;
+using Data.Forum.Interfaces.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Forum.Repository
+{
+    public class PostRepository : BaseRepository<Post> , IPostRepository
+    {
+        public PostRepository (ForumDbContext dbContext) : base(dbContext)
+        {
+
+        }
+        public Task<Post> AddAsync(Post entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Post entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(Post entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Post> IAsyncRepository<Post>.GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IReadOnlyList<Post>> IAsyncRepository<Post>.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
